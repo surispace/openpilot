bec61b2dffd29f161ae1379df92c20901f07f3e5
Suri <gallantsuri1@gmail.com>
2026-03-04 00:25:14 -0600

    suppressing laneTurnLeft laneTurnRight banners

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -312,11 +312,11 @@ class SelfdriveD(CruiseHelper):
       self.events.add(EventName.laneChange)
 
     # Handle lane turn
-    lane_turn_direction = self.sm['modelDataV2SP'].laneTurnDirection
-    if lane_turn_direction == TurnDirection.turnLeft:
-      self.events_sp.add(custom.OnroadEventSP.EventName.laneTurnLeft)
-    elif lane_turn_direction == TurnDirection.turnRight:
-      self.events_sp.add(custom.OnroadEventSP.EventName.laneTurnRight)
+    # lane_turn_direction = self.sm['modelDataV2SP'].laneTurnDirection
+    # if lane_turn_direction == TurnDirection.turnLeft:
+    #   self.events_sp.add(custom.OnroadEventSP.EventName.laneTurnLeft)
+    # elif lane_turn_direction == TurnDirection.turnRight:
+    #   self.events_sp.add(custom.OnroadEventSP.EventName.laneTurnRight)
 
     for i, pandaState in enumerate(self.sm['pandaStates']):
       # All pandas must match the list of safetyConfigs, and if outside this list, must be silent or noOutput
